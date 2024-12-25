03d50c57e4d24918bd9e5bb1c8dbacb02254d6ac
Maxime MADER <maxime.mader@kawabytes.com>
2024-12-25 01:03:21 +0100

    Day 23 and 24

--- AdventOfCode2024/AdventOfCode2024/Day23.cs
+++ AdventOfCode2024/AdventOfCode2024/Day23.cs
@@ -1,15 +1,156 @@
-// namespace AdventOfCode2024;
-//
-// public sealed class Day_26 : BaseDay
-// {
-//     private readonly string _input;
-//
-//     public Day_26()
-//     {
-//         _input = File.ReadAllText(InputFilePath);
-//     }
-//
-//     public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
-//
-//     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
-// }
+namespace AdventOfCode2024;
+
+public sealed class Day23 : BaseDay
+{
+    private readonly string _input;
+
+    public Day23()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var connections = ParseConnections();
+
+        var computersWithT = connections.Keys.Where(key => key[0] == 't').ToList();
+        
+        var hashSet = new HashSet<Triplet>();
+        
+        // Copilot's suggestion after I created my Triplet class, if it works, it works! ;)
+        foreach (var computer in computersWithT)
+        {
+            var list = connections[computer];
+            
+            foreach (var other in list)
+            {
+                if (!connections.TryGetValue(other, out var otherList)) 
+                    continue;
+                
+                foreach (var third in otherList)
+                {
+                    if (!connections.TryGetValue(third, out var thirdList)) 
+                        continue;
+                    
+                    if (thirdList.Contains(computer))
+                    {
+                        hashSet.Add(new Triplet(computer, other, third));
+                    }
+                }
+            }
+        }
+        
+        return new ValueTask<string>(hashSet.Count.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var connections = ParseConnections();
+        
+        // Found https://en.wikipedia.org/wiki/Clique_problem
+        //
+        // Bron Kerbosch algorithm
+        // https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
+        var cliques = new List<HashSet<string>>();
+        
+        var graph = connections.ToDictionary(pair => pair.Key, pair => new HashSet<string>(pair.Value));
+        
+        BronKerbosch(graph, r: [], p: [..graph.Keys], x: [], cliques);
+        
+        var result = string.Join(',', (cliques.MaxBy(c => c.Count) ?? []).Order());
+        
+        return new ValueTask<string>(result);
+    }
+    
+    private static void BronKerbosch(Dictionary<string, HashSet<string>> graph, HashSet<string> r, HashSet<string> p, HashSet<string> x,
+        List<HashSet<string>> cliques)
+    {
+        // if P and X are both empty then
+        // report R as a maximal clique
+        if (p.Count == 0 && x.Count == 0)
+        {
+            cliques.Add([..r]);
+            return;
+        }
+
+        // choose a pivot vertex u in P ⋃ X
+        var u = p.Concat(x).First();
+        
+        // for each vertex v in P \ N(u) do
+        var vNotU = new HashSet<string>(p.Except(graph[u]));
+
+        foreach (var v in vNotU)
+        {
+            var r2 = new HashSet<string>(r) { v };
+            var p2 = new HashSet<string>(p.Intersect(graph[v]));
+            var x2 = new HashSet<string>(x.Intersect(graph[v]));
+            
+            // BronKerbosch2(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+            BronKerbosch(graph, r2, p2, x2, cliques);
+            
+            // P := P \ {v}
+            p.Remove(v);
+            
+            // X := X ⋃ {v}
+            x.Add(v);
+        }
+    }
+
+    private Dictionary<string, List<string>> ParseConnections()
+    {
+        var networkMap = _input
+            .Split(Environment.NewLine)
+            .Select(line => line.Split('-'))
+            .ToArray();
+        
+        var connections = new Dictionary<string, List<string>>();
+        
+        foreach (var pair in networkMap)
+        {
+            if(connections.TryGetValue(pair[0], out var list1))
+                list1.Add(pair[1]);
+            else
+                connections[pair[0]] = [pair[1]];
+            
+            if(connections.TryGetValue(pair[1], out var list2))
+                list2.Add(pair[0]);
+            else
+                connections[pair[1]] = [pair[0]];
+        }
+
+        return connections;
+    }
+
+    private class Triplet()
+    {
+        public Triplet(string a, string b, string c) : this()
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public string A { get; }
+        public string B { get; }
+        public string C { get; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                return false;
+            
+            var other = obj as Triplet;
+            if (other == null)
+                return false;
+
+            return (A == other.A || A == other.B || A == other.C) &&
+                   (B == other.A || B == other.B || B == other.C) &&
+                   (C == other.A || C == other.B || C == other.C);
+        }
+
+        public override int GetHashCode()
+        {
+            return A.GetHashCode() + B.GetHashCode() + C.GetHashCode();
+        }
+    };
+}
--- AdventOfCode2024/AdventOfCode2024/Day24.cs
+++ AdventOfCode2024/AdventOfCode2024/Day24.cs
@@ -1,15 +1,128 @@
-// namespace AdventOfCode2024;
-//
-// public sealed class Day_26 : BaseDay
-// {
-//     private readonly string _input;
-//
-//     public Day_26()
-//     {
-//         _input = File.ReadAllText(InputFilePath);
-//     }
-//
-//     public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
-//
-//     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
-// }
+using System.Collections;
+using System.Globalization;
+using System.Numerics;
+
+namespace AdventOfCode2024;
+
+public sealed class Day24 : BaseDay
+{
+    private readonly string _input;
+
+    public Day24()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var (gates, wires) = ParseInput();
+
+        // Plain and simple, we're using a queue just to resolve the gates in a working order.
+        var queue = new Queue<(string InputA, string Op, string InputB, string Output)>(gates);
+        while (queue.TryDequeue(out var gate))
+        {
+            if (!wires.ContainsKey(gate.InputA) || !wires.ContainsKey(gate.InputB))
+            {
+                queue.Enqueue(gate);
+                continue;
+            }
+            
+            wires[gate.Output] = gate.Op switch
+            {
+                "AND" => wires[gate.InputA] & wires[gate.InputB],
+                "OR" => wires[gate.InputA] | wires[gate.InputB],
+                "XOR" => wires[gate.InputA] ^ wires[gate.InputB],
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        // No upper bound with a BigInteger.
+        var result = new BigInteger();
+        
+        foreach (var key in wires.Keys.Where(key => key.StartsWith("z")))
+        {
+            var p = int.Parse(key[1..]);
+            
+            if(wires[key])
+                result += BigInteger.Pow(2, p);
+        }
+
+        return new ValueTask<string>(result.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var (gates, _) = ParseInput();
+        
+        // We need to find which gates are faulty
+        // https://en.wikipedia.org/wiki/Adder_(electronics)
+        // https://upload.wikimedia.org/wikipedia/commons/5/57/Fulladder.gif
+        var result = new HashSet<string>();
+        
+        // The most significant bit (MSB).
+        var msbGate = gates
+            .Select(gate => gate.Output)
+            .Where(output => output.StartsWith('z'))
+            .OrderDescending()
+            .First();
+
+        // Only outputs may be incorrect, so we can check only one of the inputs.
+        foreach (var gate in gates)
+        {
+            // Every gate output that is not the MSB, should be computed from an XOR operation.
+            // The MSB should be computed from an OR operation.
+            if (IsWireZ(gate.Output) && gate.Op != "XOR" && gate.Output != msbGate)
+            {
+                result.Add(gate.Output);
+            } 
+            // Every XOR operation should either
+            // - output to a Z wire
+            // - have inputs that are an X and Y wire
+            else if (gate.Op == "XOR" && !IsWireZ(gate.Output) && !IsWireXorY(gate.InputA))
+            {
+                result.Add(gate.Output);
+            }
+            // Every X/Y inputs should be used with XOR or OR operations.
+            // For the least significant bit, we don't have a Carry input, so there's an exception to ignore.
+            else if(IsWireXorY(gate.InputA) && !IsLeastSignificantBit(gate.InputA))
+            {
+                // We need to find the next operation that should be used.
+                // Case 1: **(X XOR Y)** XOR Cin = S, we're looking for an XOR.
+                // Case 2: ((X XOR Y) AND Cin) OR **(X AND Y)** = S, we're looking for an OR.
+                if (!gates.Any(g => g != gate &&
+                            (g.InputA == gate.Output || g.InputB == gate.Output) && 
+                            g.Op == (gate.Op == "XOR" ? "XOR" : "OR")))
+                {
+                    result.Add(gate.Output);
+                }
+            }
+        }
+        
+        bool IsWireZ(string wire) => wire.StartsWith('z');
+        
+        // Unfortunately, the order of the wires aren't always InputA -> X, InputB -> Y.
+        bool IsWireXorY(string wire) => wire.StartsWith('x') || wire.StartsWith('y');
+        
+        bool IsLeastSignificantBit(string wire) => wire.EndsWith("00");
+        
+        return new ValueTask<string>(string.Join(',', result.Order()));
+    }
+    
+    private (List<(string InputA, string Op, string InputB, string Output)> gates, Dictionary<string, bool> wires) ParseInput()
+    {
+        var parts = _input.Split($"{Environment.NewLine}{Environment.NewLine}");
+
+        var wires = parts[0]
+            .Split(Environment.NewLine)
+            .Select(line => line.Split(": "))
+            .ToDictionary(p => p[0], p => int.Parse(p[1]) == 1);
+        
+        var gates = parts[1]
+            .Split(Environment.NewLine)
+            .Select(line => line.Split([" -> ", " "], StringSplitOptions.RemoveEmptyEntries))
+            .Select(gate => (InputA: gate[0], Op: gate[1], InputB: gate[2], Output: gate[3]))
+            .ToList();
+
+        return (gates, wires);
+    }
+}

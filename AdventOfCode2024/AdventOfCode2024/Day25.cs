f75e73f97dc5af9fdebc3a51750dbf25ef9f128b
Maxime MADER <maxime.mader@kawabytes.com>
2024-12-25 11:04:47 +0100

    Day 25

--- AdventOfCode2024/AdventOfCode2024/Day25.cs
+++ AdventOfCode2024/AdventOfCode2024/Day25.cs
@@ -1,15 +1,67 @@
-// namespace AdventOfCode2024;
-//
-// public sealed class Day_26 : BaseDay
-// {
-//     private readonly string _input;
-//
-//     public Day_26()
-//     {
-//         _input = File.ReadAllText(InputFilePath);
-//     }
-//
-//     public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
-//
-//     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
-// }
+namespace AdventOfCode2024;
+
+public sealed class Day25 : BaseDay
+{
+    private readonly string _input;
+
+    public Day25()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var schematics = _input.Split($"{Environment.NewLine}{Environment.NewLine}")
+            .Select(block => block.Split(Environment.NewLine))
+            .ToArray();
+        
+        var keys = new List<int[]>();
+        var locks = new List<int[]>();
+
+        foreach (var schematic in schematics)
+        {
+            if (schematic[0].All(c => c == '.'))
+            {
+                var key = new int[schematic[0].Length];
+                for (var x = 0; x < schematic[0].Length; ++x)
+                {
+                    for(var y = schematic.Length - 2; y > 0 ; --y)
+                    {
+                        if (schematic[y][x] == '.')
+                            break;
+
+                        ++key[x];
+                    }
+                }
+                keys.Add(key);
+            }
+            else if (schematic[0].All(c => c == '#'))
+            {
+                var @lock = new int[schematic[0].Length];
+                for (var x = 0; x < schematic[0].Length; ++x)
+                {
+                    for(var y = 1; y < schematic.Length - 1 ; ++y)
+                    {
+                        if (schematic[y][x] != '#')
+                            break;
+
+                        ++@lock[x];
+                    }
+                }
+                locks.Add(@lock);
+            }
+        }
+
+        var result = locks
+            .Sum(@lock => keys
+                .Count(key => !key.Where((t, i) => t + @lock[i] > 5)
+                .Any()));
+
+        return new ValueTask<string>(result.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new ValueTask<string>("Thank you, Eric");
+    }
+}
